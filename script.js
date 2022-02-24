8059117c79cc54407cf4c4141475ebb95593f954
Olivia <Oliviaparamour@gmail.com>
2022-02-24 11:04:42 +0000

    Add bling sound

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var audio = new Audio("Audio/choose_your_character.ogg");
 var audio2 = new Audio("Audio/2021-08-30_-_Boss_Time_-_www.FesliyanStudios.com.mp3");
+var audio3 = new Audio("Audio/coin1.wav");
 document.addEventListener("DOMContentLoaded", function(){
     hide_page()
 });
@@ -10,23 +11,28 @@ function play_audio() {
     audio2.currentTime=1;
     audio.play();
 };
-
+var ismute = false
 function toggle_mute() {
+    audio3.cloneNode(true).play();
     a = document.getElementById("mute")
     if (audio2.paused) {
         audio2.play();
         a.innerHTML="Mute"
         a.style = "border-color: var(--darkblue); color: var(--darkblue)";
+        ismute = false;
+        console.log(ismute)
     }
     else {
         audio2.pause();
         a.innerHTML="Unmute"
         a.style = "border-color: var(--lightblue);  color:var(--lightblue)";
+        ismute = true;
+        console.log(ismute)
     }
 }
 
 function hide_page() {
-    a = document.getElementsByClassName("instructions")[0].style.visibility = "hidden";
+    a = document.getElementById("instructions").style.visibility = "hidden";
     // b = document.getElementsByClassName("choices")[0].style.visibility = "hidden";
     c = document.getElementsByTagName("footer")[0].style.visibility = "hidden";
     b = document.getElementsByClassName("choice_logo_shape");
@@ -41,7 +47,7 @@ function hide_page() {
 };
 
 function load_page() {
-    a = document.getElementsByClassName("instructions")[0].style.visibility = "visible";
+    a = document.getElementById("instructions").style.visibility = "visible";
     // b = document.getElementsByClassName("choices")[0].style.visibility = "visible";
     c = document.getElementsByTagName("footer")[0].style.visibility = "visible";
     d = document.getElementsByClassName("starter")[0].style.visibility = "hidden";
@@ -57,3 +63,10 @@ function load_page() {
     // a.classList.add('animate__animated');
     play_audio();
 };
+
+function select_option() {
+    if (!ismute) {
+        audio3.cloneNode(true).play();
+    }
+
+}
